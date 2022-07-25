c95c3f3a69cc29fdf7f0c64bf32b7ec3a6dd6376
Mirzoyoqub Rashidov <97793909+Mirzoyo@users.noreply.github.com>
2022-07-25 21:45:56 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
-
-// ********** set date ************
-// select span
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
-// ********** close links ************
 const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function () {
-  // linksContainer.classList.toggle("show-links");
 
   const linksHeight = links.getBoundingClientRect().height;
   const containerHeight = linksContainer.getBoundingClientRect().height;
@@ -26,7 +17,6 @@ navToggle.addEventListener("click", function () {
   // console.log(linksContainer.getBoundingClientRect());
 });
 
-// ********** fixed navbar ************
 
 const navbar = document.getElementById("nav");
 const topLink = document.querySelector(".top-link");
@@ -39,7 +29,6 @@ window.addEventListener("scroll", function () {
   } else {
     navbar.classList.remove("fixed-nav");
   }
-  // setup back to top link
 
   if (scrollHeight > 500) {
     console.log("helo");
@@ -50,14 +39,10 @@ window.addEventListener("scroll", function () {
   }
 });
 
-// ********** smooth scroll ************
-// select links
 const scrollLinks = document.querySelectorAll(".scroll-link");
 scrollLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
-    // prevent default
     e.preventDefault();
-    // navigate to specific spot
     const id = e.currentTarget.getAttribute("href").slice(1);
     const element = document.getElementById(id);
 
@@ -77,8 +62,7 @@ scrollLinks.forEach((link) => {
       left: 0,
       top: position,
     });
-    // close
     linksContainer.style.height = 0;
   });
 });
-// calculate heights
+
